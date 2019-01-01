d09287676b5324e4413f79d4919f73b1180ebb3c
hetao <hetao@std.uestc.edu.cn>
2019-01-01 21:48:47 +0800

    修改了docker相关

--- docker/start.sh
+++ docker/start.sh
@@ -17,6 +17,11 @@ docker-compose rm -f
 #删除停止的容器
 #docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
 
+#删除名称为none的镜像
+docker images|grep none|awk '{print $3 }'|xargs docker rmi
+
+#删除生成的镜像
+docker rmi docker_mysql docker_app docker_nginx
 
 # 使用docker-compose启动多容器应用
 docker-compose up --build -d
